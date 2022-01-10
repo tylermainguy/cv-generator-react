d410a56e430860974774ed189c59d11dc8058d8c
Tyler Mainguy <tsmainguy@outlook.com>
2022-01-10 08:18:21 -0500

    main - #comment rewrite WorkListing and EducationListing to use functional components

--- src/components/EducationListing.js
+++ src/components/EducationListing.js
@@ -1,128 +1,117 @@
-import React, { Component } from "react";
+import React from "react";
 
-class EducationListing extends Component {
-  constructor(props) {
-    super(props);
+const EducationListing = (props) => {
+  const { education, deleteEducationEntry, editExistingEducation, toggleEdit } =
+    props;
 
-    this.state = {};
-  }
-  render() {
-    const {
-      education,
-      deleteEducationEntry,
-      editExistingEducation,
-      toggleEdit,
-    } = this.props;
+  const {
+    uniqueId,
+    location,
+    degree,
+    fieldOfStudy,
+    yearStarted,
+    yearFinished,
+    educationDescription,
+    isEdit,
+  } = education;
 
-    const {
-      uniqueId,
-      location,
-      degree,
-      fieldOfStudy,
-      yearStarted,
-      yearFinished,
-      educationDescription,
-      isEdit,
-    } = education;
-
-    if (isEdit) {
-      return (
-        <div id={uniqueId} className="edit-entry">
-          <input
-            id="location"
-            refid={uniqueId}
-            value={location}
-            onChange={editExistingEducation}
-            placeholder="Location"
-            type="text"
-          ></input>
-          <input
-            id="degree"
-            refid={uniqueId}
-            value={degree}
-            onChange={editExistingEducation}
-            placeholder="Degree (e.g. BA, MSc, PhD)"
-            type="text"
-          ></input>
-          <input
-            id="fieldOfStudy"
-            refid={uniqueId}
-            value={fieldOfStudy}
-            onChange={editExistingEducation}
-            placeholder="Field of Study (e.g. Computer Science)"
-            type="text"
-          ></input>
-          <input
-            id="yearStarted"
-            refid={uniqueId}
-            value={yearStarted}
-            onChange={editExistingEducation}
-            placeholder="Year Started"
-            type="text"
-          ></input>
-          <input
-            id="yearFinished"
-            refid={uniqueId}
-            value={yearFinished}
-            onChange={editExistingEducation}
-            placeholder="Year Finished"
-            type="text"
-          ></input>
-          <textarea
-            id="educationDescription"
-            refid={uniqueId}
-            onChange={editExistingEducation}
-            value={educationDescription}
-            placeholder="Description"
-            type="text"
-          ></textarea>
-          <button refid={uniqueId} className="add-button" onClick={toggleEdit}>
-            Resubmit
-          </button>
-        </div>
-      );
-    }
+  if (isEdit) {
     return (
       <div id={uniqueId} className="edit-entry">
-        <div>
-          <span className="education-entry-listing">Location: </span>
-          {location}
-        </div>
-        <div>
-          <span className="education-entry-listing">Degree: </span>
-          {degree}
-        </div>
-        <div>
-          <span className="education-entry-listing">Field of Study: </span>
-          {fieldOfStudy}
-        </div>
-        <div>
-          <span className="education-entry-listing">Year Started: </span>
-          {yearStarted}
-        </div>
-        <div>
-          <span className="education-entry-listing">Year Finished: </span>
-          {yearFinished}
-        </div>
-        <div>
-          <span className="education-entry-listing">Description: </span>
-          {educationDescription}
-        </div>
-        <div className="listing-buttons">
-          <button refid={uniqueId} className="edit-button" onClick={toggleEdit}>
-            Edit
-          </button>
-          <button
-            refid={uniqueId}
-            className="delete-button"
-            onClick={deleteEducationEntry}
-          >
-            Delete
-          </button>
-        </div>
+        <input
+          id="location"
+          refid={uniqueId}
+          value={location}
+          onChange={editExistingEducation}
+          placeholder="Location"
+          type="text"
+        ></input>
+        <input
+          id="degree"
+          refid={uniqueId}
+          value={degree}
+          onChange={editExistingEducation}
+          placeholder="Degree (e.g. BA, MSc, PhD)"
+          type="text"
+        ></input>
+        <input
+          id="fieldOfStudy"
+          refid={uniqueId}
+          value={fieldOfStudy}
+          onChange={editExistingEducation}
+          placeholder="Field of Study (e.g. Computer Science)"
+          type="text"
+        ></input>
+        <input
+          id="yearStarted"
+          refid={uniqueId}
+          value={yearStarted}
+          onChange={editExistingEducation}
+          placeholder="Year Started"
+          type="text"
+        ></input>
+        <input
+          id="yearFinished"
+          refid={uniqueId}
+          value={yearFinished}
+          onChange={editExistingEducation}
+          placeholder="Year Finished"
+          type="text"
+        ></input>
+        <textarea
+          id="educationDescription"
+          refid={uniqueId}
+          onChange={editExistingEducation}
+          value={educationDescription}
+          placeholder="Description"
+          type="text"
+        ></textarea>
+        <button refid={uniqueId} className="add-button" onClick={toggleEdit}>
+          Resubmit
+        </button>
       </div>
     );
   }
-}
+  return (
+    <div id={uniqueId} className="edit-entry">
+      <div>
+        <span className="education-entry-listing">Location: </span>
+        {location}
+      </div>
+      <div>
+        <span className="education-entry-listing">Degree: </span>
+        {degree}
+      </div>
+      <div>
+        <span className="education-entry-listing">Field of Study: </span>
+        {fieldOfStudy}
+      </div>
+      <div>
+        <span className="education-entry-listing">Year Started: </span>
+        {yearStarted}
+      </div>
+      <div>
+        <span className="education-entry-listing">Year Finished: </span>
+        {yearFinished}
+      </div>
+      <div>
+        <span className="education-entry-listing">Description: </span>
+        {educationDescription}
+      </div>
+      <div className="listing-buttons">
+        <button refid={uniqueId} className="edit-button" onClick={toggleEdit}>
+          Edit
+        </button>
+        <button
+          refid={uniqueId}
+          className="delete-button"
+          onClick={deleteEducationEntry}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default EducationListing;
--- src/components/WorkListing.js
+++ src/components/WorkListing.js
@@ -1,111 +1,103 @@
-import React, { Component } from "react";
+import React from "react";
 
-class WorkListing extends Component {
-  constructor(props) {
-    super(props);
+const WorkListing = (props) => {
+  const { work, deleteEntry, toggleEdit, editExistingWork } = props;
+  const {
+    uniqueId,
+    company,
+    position,
+    workStarted,
+    workFinished,
+    workDescription,
+    isEdit,
+  } = work;
 
-    this.state = {};
-  }
-
-  render() {
-    const { work, deleteEntry, toggleEdit, editExistingWork } = this.props;
-    const {
-      uniqueId,
-      company,
-      position,
-      workStarted,
-      workFinished,
-      workDescription,
-      isEdit,
-    } = work;
-
-    if (isEdit) {
-      return (
-        <div id={uniqueId} className="edit-entry">
-          <input
-            id="company"
-            placeholder="Company Name"
-            value={company}
-            onChange={editExistingWork}
-            refid={uniqueId}
-            type="text"
-          ></input>
-          <input
-            id="position"
-            placeholder="Position"
-            onChange={editExistingWork}
-            refid={uniqueId}
-            value={position}
-            type="text"
-          ></input>
-          <input
-            id="workStarted"
-            placeholder="Year Started"
-            value={workStarted}
-            refid={uniqueId}
-            onChange={editExistingWork}
-            type="text"
-          ></input>
-          <input
-            id="workFinished"
-            placeholder="Year Finished"
-            refid={uniqueId}
-            value={workFinished}
-            onChange={editExistingWork}
-            type="text"
-          ></input>
-          <textarea
-            id="workDescription"
-            placeholder="Description"
-            refid={uniqueId}
-            onChange={editExistingWork}
-            value={workDescription}
-            type="text"
-          ></textarea>
-          <button refid={uniqueId} className="add-button" onClick={toggleEdit}>
-            Resubmit
-          </button>
-        </div>
-      );
-    }
-
+  if (isEdit) {
     return (
       <div id={uniqueId} className="edit-entry">
-        <div>
-          <span className="work-entry-listing">Company: </span>
-          {company}
-        </div>
-        <div>
-          <span className="work-entry-listing">Position: </span>
-          {position}
-        </div>
-        <div>
-          <span className="work-entry-listing">Year Started: </span>
-          {workStarted}
-        </div>
-        <div>
-          <span className="work-entry-listing">Year Finished: </span>
-          {workFinished}
-        </div>
-        <div>
-          <span className="work-entry-listing">Description: </span>
-          {workDescription}
-        </div>
-        <div className="listing-buttons">
-          <button refid={uniqueId} className="edit-button" onClick={toggleEdit}>
-            Edit
-          </button>
-          <button
-            refid={uniqueId}
-            className="delete-button"
-            onClick={deleteEntry}
-          >
-            Delete
-          </button>
-        </div>
+        <input
+          id="company"
+          placeholder="Company Name"
+          value={company}
+          onChange={editExistingWork}
+          refid={uniqueId}
+          type="text"
+        ></input>
+        <input
+          id="position"
+          placeholder="Position"
+          onChange={editExistingWork}
+          refid={uniqueId}
+          value={position}
+          type="text"
+        ></input>
+        <input
+          id="workStarted"
+          placeholder="Year Started"
+          value={workStarted}
+          refid={uniqueId}
+          onChange={editExistingWork}
+          type="text"
+        ></input>
+        <input
+          id="workFinished"
+          placeholder="Year Finished"
+          refid={uniqueId}
+          value={workFinished}
+          onChange={editExistingWork}
+          type="text"
+        ></input>
+        <textarea
+          id="workDescription"
+          placeholder="Description"
+          refid={uniqueId}
+          onChange={editExistingWork}
+          value={workDescription}
+          type="text"
+        ></textarea>
+        <button refid={uniqueId} className="add-button" onClick={toggleEdit}>
+          Resubmit
+        </button>
       </div>
     );
   }
-}
+
+  return (
+    <div id={uniqueId} className="edit-entry">
+      <div>
+        <span className="work-entry-listing">Company: </span>
+        {company}
+      </div>
+      <div>
+        <span className="work-entry-listing">Position: </span>
+        {position}
+      </div>
+      <div>
+        <span className="work-entry-listing">Year Started: </span>
+        {workStarted}
+      </div>
+      <div>
+        <span className="work-entry-listing">Year Finished: </span>
+        {workFinished}
+      </div>
+      <div>
+        <span className="work-entry-listing">Description: </span>
+        {workDescription}
+      </div>
+      <div className="listing-buttons">
+        <button refid={uniqueId} className="edit-button" onClick={toggleEdit}>
+          Edit
+        </button>
+        <button
+          refid={uniqueId}
+          className="delete-button"
+          onClick={deleteEntry}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default WorkListing;
